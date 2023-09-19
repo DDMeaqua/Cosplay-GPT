f0564aee4cf1097384b20fce95b9088570b858c2
Meaqua <lzlloveyou@vip.qq.com>
2023-09-20 00:06:59 +0800

    fix: bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
 					fontSans.variable
 				)}
 			>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
+				<Providers>
 					{children}
 				</Providers>
 			</body>
